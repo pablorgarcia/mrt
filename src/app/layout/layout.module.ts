0846b931a9d4384a50af059fa62bd9e0ad633af8
pablorgarcia <pablo.ramon.garcia.fernandez@gmail.com>
2021-06-15 00:39:14 +0200

    working on profile display grid

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { PagesModule } from '../pages/pages.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './header/header.component';
@@ -21,7 +22,8 @@ import { DriversListComponent } from './drivers-list/drivers-list.component';
   imports: [
     CommonModule,
     LayoutRoutingModule,
-    PagesModule
+    PagesModule,
+    ComponentsModule
   ]
 })
 export class LayoutModule {}
