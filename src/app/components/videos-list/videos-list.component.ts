14c70d3a426809544673d67e56364bdd0e207f5a
pablorgarcia <pablo.ramon.garcia.fernandez@gmail.com>
2021-06-16 01:42:44 +0200

    working on video list res

--- src/app/components/videos-list/videos-list.component.ts
+++ src/app/components/videos-list/videos-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
-// PROBANDO
-import { VideoService } from '../../services/video.service';
 import { VideoModel } from '../../models/VideoModel';
+
+import { VideoService } from '../../services/video.service';
+
 
 @Component({
   selector: 'app-videos-list',
@@ -11,203 +12,31 @@ import { VideoModel } from '../../models/VideoModel';
 })
 export class VideosListComponent implements OnInit {
 
-  // PROBANDO
-  public videoData: VideoModel | undefined;
+  @Input() public lastVideos: boolean = false;
+  public lastVideoData: any | undefined;
+
+  @Input() public hotLapVideos: boolean = false;
+  public hotLapVideoData: any | undefined;
+
 
   constructor(private videoService: VideoService) {}
 
   ngOnInit(): void {
-    this.videoService.getLastVideos().subscribe((res: VideoModel) => {
-      console.log(res)
-      this.videoData = res;
-    });
-    //const videoData: VideoModel[] = VideoService.getLastVideos();
+    console.log('hey false? ', this.lastVideos)
+
+    if (this.lastVideos) {
+      this.videoService.getLastVideos().subscribe((res: VideoModel) => {
+        console.log('getLastVideos', res)
+        this.lastVideoData = res;
+      });
+    }
+
+    if (this.hotLapVideos) {
+      console.log('cargamos los videos de HOTLAPs')
+    }
+
   }
 
-}
 
-/* RESULTADO DE LA LLAMADA DE LA API
 
-{
-  kind: "youtube#searchListResponse",
-    etag: "ygVeVuJt6a7fCdwuWWPrLfTF4I4",
-      nextPageToken: "CAUQAA",
-        regionCode: "ES",
-          pageInfo: {
-    totalResults: 101,
-      resultsPerPage: 5
-  },
-  items: [
-    {
-      kind: "youtube#searchResult",
-      etag: "DQrIq_1i2ggRpditk56sJ8OxGzU",
-      id: {
-        kind: "youtube#video",
-        videoId: "t61C56vTAnY"
-      },
-      snippet: {
-        publishedAt: "2021-06-06T07:00:12Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "Hot lap Pinto Karting 56,591",
-        description: "Karting Pinto, Madrid [17/04/21] [Hot lap] [onboard] [Pinto Karting] Hotlap: 56591 Intro: 0:00 Hotlap: 0:18 Ending: 01:19 Pablo García, racing driver of MRT ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/t61C56vTAnY/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/t61C56vTAnY/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/t61C56vTAnY/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2021-06-06T07:00:12Z"
-      }
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "aXajHu3eKhAJddvQwzJpRf14KlU",
-      id: {
-        kind: "youtube#video",
-        videoId: "u9e6b-1NXME"
-      },
-      snippet: {
-        publishedAt: "2021-06-05T07:00:12Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "GP3 Endurance Pinto Karting with Dino kart [Karting race]",
-        description: "GP3 1h Dinos championship Karting Pinto: 80kg [29/05/21] [onboard] [Karting race] Hot lap: 56591 - Karting Pinto Intro: 0:00 Race: 0:16 Ending: 57:42 Pablo ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/u9e6b-1NXME/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/u9e6b-1NXME/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/u9e6b-1NXME/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2021-06-05T07:00:12Z"
-      }
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "_l3rh_eCCb8xKPxDwyWoSuoGiow",
-      id: {
-        kind: "youtube#video",
-        videoId: "SR_VhcoYjQ0"
-      },
-      snippet: {
-        publishedAt: "2021-05-09T07:00:07Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "GP2 Endurance Pinto Karting with Dino kart [Karting race]",
-        description: "GP2 1h Dinos championship Karting Pinto: 90kg [17/04/21] [onboard] [Karting race] Hotlap: 56920 - Karting Pinto Intro: 0:00 Race: 0:16 Ending: 56:20 Pablo ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/SR_VhcoYjQ0/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/SR_VhcoYjQ0/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/SR_VhcoYjQ0/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2021-05-09T07:00:07Z"
-      }
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "UI8YwEOvEzjV74e-blkR-AYYOY0",
-      id: {
-        kind: "youtube#video",
-        videoId: "yTaDXUdq4AU"
-      },
-      snippet: {
-        publishedAt: "2021-05-08T07:00:15Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "Hot lap Pinto Karting 56,920",
-        description: "Karting Pinto, Madrid [17/04/21] [Hot lap] [onboard] [Pinto Karting] Hotlap: 56920 Intro: 0:00 Hotlap: 0:18 Ending: 01:34 Pablo García, racing driver of MRT ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/yTaDXUdq4AU/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/yTaDXUdq4AU/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/yTaDXUdq4AU/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2021-05-08T07:00:15Z"
-      }
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "ryGdY0G-WvBSsl97lyWvE3W3z9Q",
-      id: {
-        kind: "youtube#video",
-        videoId: "m-4CcKKMaA8"
-      },
-      snippet: {
-        publishedAt: "2020-12-20T08:30:06Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "Karting DR7 [Track day]",
-        description: "First time in the Circuito DR7, Tarancon, Cuenca, Spain [Track day] [26/06/19] [onboard] Circuito Daniel Rivas 7 Intro: 0:00 Track day: 0:18 Ending: 04:47 Pablo ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/m-4CcKKMaA8/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/m-4CcKKMaA8/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/m-4CcKKMaA8/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2020-12-20T08:30:06Z"
-      }
-    }
-  ]
 }
-*/
--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-// PROBANDO
 import { YOUTUBE_CONF } from '../../config';
 
 @Injectable({
@@ -10,226 +9,22 @@ import { YOUTUBE_CONF } from '../../config';
 })
 export class VideoService {
 
-  public youtubeURLbase2: string = 'https://www.googleapis.com/youtube/v3/'
-
-  constructor(private http: HttpClient) {
-    console.log('constructor VIDEO SERVICE')
-  }
-
   // https://developers.google.com/youtube/v3
   // https://developers.google.com/youtube/v3/docs/search/list?hl=es#javascript
 
+  constructor(private http: HttpClient) {}
 
   private static urlLastVideos(): string {
-    console.log('urlLastVideos() VIDEOS SERVICE')
-    //PORQUÉ 'public youtubeURLbase2' está fuera del scope de esta funcion ???
-    //const youtubeURLbase: string = 'https://www.googleapis.com/youtube/v3/'
-    //return `${youtubeURLbase}search?key=${YOUTUBE_CONF.API_KEY}&channelId=${YOUTUBE_CONF.CHANNEL_ID}&part=snippet,id&order=date&maxResults=5`;
     return `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_CONF.API_KEY}&channelId=${YOUTUBE_CONF.CHANNEL_ID}&part=snippet,id&order=date&maxResults=5`;
   }
 
   private static urlYoutubeList(): string {
-    console.log('urlYoutubeList() VIDEOS SERVICE')
     // QUÉ LISTAS SALEN AQUÍ ???
     return `https://www.googleapis.com/youtube/v3/playlists?key=${YOUTUBE_CONF.API_KEY}&channelId=${YOUTUBE_CONF.CHANNEL_ID}`;
   }
 
+  public getLastVideos(): Observable<any> { return this.http.get(VideoService.urlLastVideos()); }
 
-  public getLastVideos(): Observable<any> {
-    console.log('getVideos() VIDEOS SERVICE')
-    return this.http.get(VideoService.urlLastVideos());
-  }
-
-  public getYoutubeList(): Observable<any> {
-    console.log('getYoutubeList() VIDEOS SERVICE')
-    return this.http.get(VideoService.urlYoutubeList());
-  }
+  public getYoutubeList(): Observable<any> { return this.http.get(VideoService.urlYoutubeList()); }
 
 }
-
-
-/* RESULTADO DE LA LLAMADA DE LA API LAST VIDEOS
-
-{
-  kind: "youtube#searchListResponse",
-    etag: "ygVeVuJt6a7fCdwuWWPrLfTF4I4",
-      nextPageToken: "CAUQAA",
-        regionCode: "ES",
-          pageInfo: {
-    totalResults: 101,
-      resultsPerPage: 5
-  },
-  items: [
-    {
-      kind: "youtube#searchResult",
-      etag: "DQrIq_1i2ggRpditk56sJ8OxGzU",
-      id: {
-        kind: "youtube#video",
-        videoId: "t61C56vTAnY"
-      },
-      snippet: {
-        publishedAt: "2021-06-06T07:00:12Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "Hot lap Pinto Karting 56,591",
-        description: "Karting Pinto, Madrid [17/04/21] [Hot lap] [onboard] [Pinto Karting] Hotlap: 56591 Intro: 0:00 Hotlap: 0:18 Ending: 01:19 Pablo García, racing driver of MRT ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/t61C56vTAnY/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/t61C56vTAnY/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/t61C56vTAnY/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2021-06-06T07:00:12Z"
-      }
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "aXajHu3eKhAJddvQwzJpRf14KlU",
-      id: {
-        kind: "youtube#video",
-        videoId: "u9e6b-1NXME"
-      },
-      snippet: {
-        publishedAt: "2021-06-05T07:00:12Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "GP3 Endurance Pinto Karting with Dino kart [Karting race]",
-        description: "GP3 1h Dinos championship Karting Pinto: 80kg [29/05/21] [onboard] [Karting race] Hot lap: 56591 - Karting Pinto Intro: 0:00 Race: 0:16 Ending: 57:42 Pablo ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/u9e6b-1NXME/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/u9e6b-1NXME/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/u9e6b-1NXME/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2021-06-05T07:00:12Z"
-      }
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "_l3rh_eCCb8xKPxDwyWoSuoGiow",
-      id: {
-        kind: "youtube#video",
-        videoId: "SR_VhcoYjQ0"
-      },
-      snippet: {
-        publishedAt: "2021-05-09T07:00:07Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "GP2 Endurance Pinto Karting with Dino kart [Karting race]",
-        description: "GP2 1h Dinos championship Karting Pinto: 90kg [17/04/21] [onboard] [Karting race] Hotlap: 56920 - Karting Pinto Intro: 0:00 Race: 0:16 Ending: 56:20 Pablo ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/SR_VhcoYjQ0/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/SR_VhcoYjQ0/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/SR_VhcoYjQ0/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2021-05-09T07:00:07Z"
-      }
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "UI8YwEOvEzjV74e-blkR-AYYOY0",
-      id: {
-        kind: "youtube#video",
-        videoId: "yTaDXUdq4AU"
-      },
-      snippet: {
-        publishedAt: "2021-05-08T07:00:15Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "Hot lap Pinto Karting 56,920",
-        description: "Karting Pinto, Madrid [17/04/21] [Hot lap] [onboard] [Pinto Karting] Hotlap: 56920 Intro: 0:00 Hotlap: 0:18 Ending: 01:34 Pablo García, racing driver of MRT ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/yTaDXUdq4AU/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/yTaDXUdq4AU/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/yTaDXUdq4AU/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2021-05-08T07:00:15Z"
-      }
-    },
-    {
-      kind: "youtube#searchResult",
-      etag: "ryGdY0G-WvBSsl97lyWvE3W3z9Q",
-      id: {
-        kind: "youtube#video",
-        videoId: "m-4CcKKMaA8"
-      },
-      snippet: {
-        publishedAt: "2020-12-20T08:30:06Z",
-        channelId: "UCAnEUW12DYwZ8kgET9HXDKw",
-        title: "Karting DR7 [Track day]",
-        description: "First time in the Circuito DR7, Tarancon, Cuenca, Spain [Track day] [26/06/19] [onboard] Circuito Daniel Rivas 7 Intro: 0:00 Track day: 0:18 Ending: 04:47 Pablo ...",
-        thumbnails: {
-          default: {
-            url: "https://i.ytimg.com/vi/m-4CcKKMaA8/default.jpg",
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: "https://i.ytimg.com/vi/m-4CcKKMaA8/mqdefault.jpg",
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: "https://i.ytimg.com/vi/m-4CcKKMaA8/hqdefault.jpg",
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: "Pablo García",
-        liveBroadcastContent: "none",
-        publishTime: "2020-12-20T08:30:06Z"
-      }
-    }
-  ]
-}
-*/
